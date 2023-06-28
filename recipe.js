ae4895a7c55aa681213a2250c743e0be085ed219
Jenny Zhang <95259224+jxnnyzhang@users.noreply.github.com>
2023-06-28 16:51:09 -0400

    Update recipe.js

--- recipe.js
+++ recipe.js
@@ -11,66 +11,58 @@ const app = express();
 app.use(bodyParser.json());
 
 // Define the recipes variable at the top level
-let recipes = []; 
+let recipes = [];
 
+// Define the webhook endpoint for Dialogflow
+app.post('/dialogflow-webhook', async (req, res) => {
+  try {
+    // Extract the user input from the Dialogflow request
+    const intent = req.body.queryResult?.intent?.displayName;
+    const userInput = req.body.queryResult?.parameters?.recipe;
+    const restrictions = req.body.queryResult?.parameters?.restrictions;
 
-// Define the webhook endpoint
-app.post('/webhook', (req, res) => {
-  // Extract the user input from the Dialogflow request
-  const intent = req.body.queryResult?.intent?.displayName;
-  const userInput = req.body.queryResult?.parameters?.recipe;
-  const restrictions = req.body.queryResult?.parameters?.restrictions;
+    if (intent === 'RecipeFinder') {
+      // Call the processUserInput function with the user input
+      const formattedResponse = await processUserInput(userInput);
 
-  if (intent === 'RecipeFinder') {
-    // Call the processUserInput function with the user input
-    processUserInput(userInput)
-      .then((formattedResponse) => {
-        // Send the response back to Dialogflow
-        res.json({
-          fulfillmentText: formattedResponse.fulfillmentText,
-          fulfillmentMessages: formattedResponse.fulfillmentMessages,
-        });
-      })
-      .catch((error) => {
-        console.error('Error:', error.message);
-        // Handle and format the error response
-        const errorResponse = formatErrorResponse(error);
-
-        // Send the error response back to Dialogflow
-        res.json(errorResponse);
+      // Send the response back to Dialogflow
+      res.json({
+        fulfillmentText: formattedResponse.fulfillmentText,
+        fulfillmentMessages: formattedResponse.fulfillmentMessages,
       });
-  } else if (intent === 'Restrictions') {
+    } else if (intent === 'Restrictions') {
+      // Process the restrictions and perform necessary actions
+      // For example, you can filter recipes based on the given restrictions
+      const filteredRecipes = filterRecipesByRestrictions(recipes, restrictions);
 
-    // Process the restrictions and perform necessary actions
-    // For example, you can filter recipes based on the given restrictions
-    const filteredRecipes = filterRecipesByRestrictions(recipes, restrictions);
+      // Prepare the response message
+      const responseText = `Recipes filtered by restrictions: ${filteredRecipes.length}`;
 
-    // Prepare the response message
-    const responseText = `Recipes filtered by restrictions: ${filteredRecipes.length}`;
-
-    // Send the response back to Dialogflow
-    res.json({
-      fulfillmentText: responseText,
-      fulfillmentMessages: [
-        {
-          text: {
-            text: [responseText],
+      // Send the response back to Dialogflow
+      res.json({
+        fulfillmentText: responseText,
+        fulfillmentMessages: [
+          {
+            text: {
+              text: [responseText],
+            },
           },
-        },
-      ],
-    });
+        ],
+      });
+    }
+  } catch (error) {
+    console.error('Error:', error.message);
+    // Handle and format the error response
+    const errorResponse = formatErrorResponse(error);
+
+    // Send the error response back to Dialogflow
+    res.json(errorResponse);
   }
 });
 
-// Start the Express server
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
-});
-
-
-// Function to handle user inputs and process queries
-async function processUserInput(userInput) {
-  // Make a request to the Spoonacular API
+// Define the endpoint for recipe search
+app.get('/search-recipes', async (req, res) => {
+  const userInput = req.query.q;
   try {
     const response = await axios.get('https://api.edamam.com/search', {
       params: {
@@ -86,66 +78,74 @@ async function processUserInput(userInput) {
     console.log('Response data structure:', Object.keys(response.data));
 
     // Extract the recipe results from the API response
-    const recipes = response.data.hits.map((hit) => hit.recipe);
+    recipes = response.data.hits.map((hit) => hit.recipe);
     console.log('Recipes:', recipes);
-    const formattedResponse = processRecipesAndFormatResponse(recipes);
-    return formattedResponse;
+    res.json({ success: true });
+  } catch (error) {
+    console.error('Error retrieving recipes:', error);
+    res.status(500).json({ success: false, error: 'Failed to retrieve recipes. Please try again.' });
+  }
+});
+
+// Start the Express server
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
+
+// Function to handle user inputs and process queries
+async function processUserInput(userInput) {
+  try {
+    const response = await axios.get('http://localhost:3000/search-recipes', {
+      params: {
+        q: userInput,
+      },
+    });
+
+    // Prepare the response message
+    const responseText = `Found ${response.data.success ? recipes.length : '0'} recipes for "${userInput}"`;
+
+    // Send the response back to Dialogflow
+    return {
+      fulfillmentText: responseText,
+      fulfillmentMessages: [
+        {
+          text: {
+            text: [responseText],
+          },
+        },
+      ],
+    };
   } catch (error) {
     console.error('Error retrieving recipes:', error);
     throw new Error('Failed to retrieve recipes. Please try again.');
   }
 }
 
+// Function to filter recipes based on restrictions
 function filterRecipesByRestrictions(recipes, restrictions) {
-  // Assuming you have an array of recipes available, you can filter them based on the given restrictions
-  const filteredRecipes = recipes.filter((recipes) => {
-    // Check if the recipe meets all the restrictions
-    return restrictions.every((restrictions) => {
-      // Implement the logic to check if the recipe meets a specific restriction
-      // For example, if the restriction is "gluten-free", check if the recipe is gluten-free
-      // If the restriction is "vegetarian", check if the recipe is vegetarian
-      // Return true if the recipe meets the restriction, otherwise return false
-      return recipes.tags.includes(restrictions);
-    });
+  // Implement the logic to filter recipes based on restrictions
+  // For example, you can iterate over the recipes and check if they match the given restrictions
+  const filteredRecipes = recipes.filter((recipe) => {
+    // Implement your restriction checking logic here
+    // For example, check if the recipe ingredients contain any restricted ingredient
+    return !recipe.ingredients.some((ingredient) => restrictions.includes(ingredient));
   });
+
   return filteredRecipes;
 }
 
-
-// Function to process the recipes and format the response
-function processRecipesAndFormatResponse(recipes) {
-  // Process the recipes and return the formatted response
-  const processedRecipes = recipes.map((recipe) => {
-    return {
-      title: recipe.label,
-      ingredients: recipe.ingredientLines,
-      nutritionalFacts: recipe.totalNutrients,
-    };
-  });
-
-  return {
-    fulfillmentText: 'Recipes found:',
-    fulfillmentMessages: processedRecipes.map((recipe) => ({
-      text: {
-        text: [
-          `Title: ${recipe.title}`,
-          'Ingredients:',
-          ...recipe.ingredients,
-          'Nutritional Facts:',
-          JSON.stringify(recipe.nutritionalFacts),
-        ],
-      },
-    })),
-  };
-}
-
-// Function to format the error response
+// Function to format error responses
 function formatErrorResponse(error) {
-  // Format the error response according to your desired format
-  const errorResponse = {
-    fulfillmentText: 'An error occurred: ' + error.message,
+  return {
+    fulfillmentText: `An error occurred: ${error.message}`,
+    fulfillmentMessages: [
+      {
+        text: {
+          text: [`An error occurred: ${error.message}`],
+        },
+      },
+    ],
   };
-  return errorResponse;
 }
 
 
